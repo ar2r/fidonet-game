5ffe1472d7ad5df9605b0dd9b2bbd55f0c47adca
Artur Khasanov <artur@hasanov.ru>
2026-02-07 17:10:03 +0300

    Test: Mock AudioContext in setupTests.js
    - Added mock for window.AudioContext to fix tests failing with 'TypeError: AudioContext is not a constructor'.

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,3 +2,30 @@ import '@testing-library/jest-dom';
 
 // JSDOM doesn't implement scrollIntoView
 window.HTMLElement.prototype.scrollIntoView = function () { };
+
+// Mock Web Audio API
+window.AudioContext = class AudioContext {
+    constructor() {
+        this.state = 'running';
+    }
+    createOscillator() {
+        return {
+            connect: () => {},
+            start: () => {},
+            stop: () => {},
+            type: 'sine',
+            frequency: { value: 440, setValueAtTime: () => {}, linearRampToValueAtTime: () => {} }
+        };
+    }
+    createGain() {
+        return {
+            connect: () => {},
+            gain: { value: 1, setValueAtTime: () => {}, exponentialRampToValueAtTime: () => {} }
+        };
+    }
+    suspend() { this.state = 'suspended'; }
+    resume() { this.state = 'running'; }
+    get currentTime() { return 0; }
+    get destination() { return {}; }
+};
+window.webkitAudioContext = window.AudioContext;
