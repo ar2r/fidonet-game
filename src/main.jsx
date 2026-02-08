eb8b03c8e27c9d9ab95ed5db9cdff419daefcb19
Artur Khasanov <artur@hasanov.ru>
2026-02-08 21:36:13 +0300

    fix: inject DosVga font via JS to resolve loading issues

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,19 @@ import { Provider } from 'react-redux'
 import { store } from './engine/store'
 import './index.css'
 import App from './App.jsx'
+import DosVgaFont from './assets/fonts/IBM_VGA_9x16.ttf'
+
+// Inject Font
+const fontStyle = document.createElement('style');
+fontStyle.textContent = `
+  @font-face {
+    font-family: 'DosVga';
+    src: url('${DosVgaFont}') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+  }
+`;
+document.head.appendChild(fontStyle);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
