4814cef9962d8d9a4cd90e43a8556512053fb904
Artur Khasanov <artur@hasanov.ru>
2026-02-08 21:43:17 +0300

    style: revert external font usage and switch to standard Courier font

--- src/main.jsx
+++ src/main.jsx
@@ -4,19 +4,6 @@ import { Provider } from 'react-redux'
 import { store } from './engine/store'
 import './index.css'
 import App from './App.jsx'
-import DosVgaFontWoff from './assets/fonts/PxPlus_IBM_VGA_9x16.woff'
-
-// Inject Font
-const fontStyle = document.createElement('style');
-fontStyle.textContent = `
-  @font-face {
-    font-family: 'DosVga';
-    src: url('${DosVgaFontWoff}') format('woff');
-    font-weight: normal;
-    font-style: normal;
-  }
-`;
-document.head.appendChild(fontStyle);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
